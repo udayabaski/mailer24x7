cac81e462c8d9a6490dacd2ea5af8fe0d0e06755
baskar.sks <baskar.sks@gmail.com>
2012-11-17 16:38:07 +0000

    Initial import.

---
+++ mailer24x7-Beta/src/com/nervytech/mailer24x7/model/service/impl/UserSessionService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nervytech.mailer24x7.model.service.impl;
+
+import com.nervytech.mailer24x7.model.service.api.IUserSessionService;
+
+/**
+ * @author bsikkaya
+ *
+ */
+public class UserSessionService implements IUserSessionService{
+
+}
