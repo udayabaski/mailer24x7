d587c6b701d110d3fd876e1bf731777f299a0de4
baskar.sks@gmail.com <baskar.sks@gmail.com@c9c8a45a-7273-3f96-ea41-065c1cc91c21>
2012-11-17 16:36:55 +0000

    Initial import.

---
+++ trunk/mailer24x7-Beta/src/com/nervytech/mailer24x7/spring/security/auth/user/UserDetailsServiceImpl.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package com.nervytech.mailer24x7.spring.security.auth.user;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.nervytech.mailer24x7.common.enums.UserRoleEnum;
+import com.nervytech.mailer24x7.common.enums.UserStatusEnum;
+import com.nervytech.mailer24x7.domain.model.User;
+import com.nervytech.mailer24x7.model.dao.interfaces.IUserDAO;
+
+/**
+ * @author ADMIN
+ * 
+ */
+@Service("userDetailsService")
+@SessionAttributes("user")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserDetailsServiceImpl.class);
+
+	@Autowired
+	private IUserDAO usersDAO;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException, DataAccessException {
+
+		SessionUser springUser = null;
+		User user = null;
+		try {
+			List<User> users = usersDAO.getUser(username);
+			if (users.size() <= 0) {
+				
+				logger.info("User "+username+" not found in the system.");
+				
+				throw new UsernameNotFoundException(
+						"Oops!!! Invalid username.");
+				
+			}
+			user = (User) users.get(0);
+
+			Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			/*
+			 * for (UserRoles role : user.getUserRoleses()) {
+			 * authorities.add(new GrantedAuthorityImpl(role.getRoleName())); }
+			 */
+			authorities.add(new GrantedAuthorityImpl(UserRoleEnum.values()[user
+					.getRole()].toString()));
+
+			// User springUser = new User(userName, user.getPassword(), true,
+			// true, true, true, authorities);
+
+			boolean enabled = false;
+			if (user.getStatus() == UserStatusEnum.ENABLED.getStatus()) {
+				enabled = true;
+			}
+
+			springUser = new SessionUser(username, user.getPassword(),
+					authorities, user.getOrgId(), user.getUserId(), enabled);
+
+		} catch (Exception e) {
+			
+			logger.error("Error while checking if user exist : ", e);
+			throw new UsernameNotFoundException("Error while checking if user exist !!!");
+		}
+		
+		return springUser;
+	}
+
+	public static SessionUser currentUserDetails() {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authentication = securityContext.getAuthentication();
+		if (authentication != null) {
+			Object principal = authentication.getPrincipal();
+			return principal instanceof UserDetails ? (SessionUser) principal
+					: null;
+		}
+		return null;
+	}
+
+}
